d1322138f66155d118b00aca1bcfb173994ad3fb
JRSP <JRSP@192.168.1.129>
2016-10-14 13:52:55 +0200

    modified userDao

--- src/main/java/daos/SportDao.java
+++ src/main/java/daos/SportDao.java
@@ -2,8 +2,6 @@ package daos;
 
 import java.util.List;
 
-import es.upm.miw.apiArchitectureTheme.entities.Vote;
-
 public interface SportDao extends GenericDao<Sport, String> {
 
 }
--- src/main/java/daos/UserDao.java
+++ src/main/java/daos/UserDao.java
@@ -2,4 +2,6 @@ package daos;
 
 
 public interface UserDao extends GenericDao<User, String> {
+    public abstract void addSport(String user, String sport);
+    public abstract void usersBySport(String sport);
 }
